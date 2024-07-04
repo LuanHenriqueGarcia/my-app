8bffebbabc0908005656c720a9c995e77fd4630a
Luan garcia <luangarciatodero2007@gmail.com>
2024-07-04 10:47:46 -0300

    arrumando-erros

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { EmojiRating } from "@/components/EmojiRating";
 const Page = () => {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <EmojiRating  rate={2.2} />
+      <EmojiRating  rate={5.2} />
 
     </div>
   )
